1125465be4b2440ba7d7c40c2e3fc4e7765bba23
Luis <64982212+Ltotheois@users.noreply.github.com>
2021-02-03 01:01:19 +0100

    Initial Commit

---
+++ resources/download/InstallScripts/ubuntu_install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+apt-get install python3
+apt-get install python3-pip
+python3 -m pip install pandas
+python3 -m pip install scipy
+python3 -m pip install numpy
+python3 -m pip install matplotlib
+python3 -m pip install wrapt
+python3 -m pip install pyqt5
+apt-get install python3-pyqt5
+
+echo "Finished"
---
+++ resources/download/InstallScripts/windows_install.bat
@@ -0,0 +1,8 @@
+pip3 install pandas
+pip3 install numpy
+pip3 install scipy
+pip3 install wrapt
+pip3 install matplotlib
+pip3 install pyqt5
+ECHO "FINISHED, CHECK FOR ERRORS IN OUTPUT, THEN PRESS ENTER TO CLOSE"
+PAUSE
