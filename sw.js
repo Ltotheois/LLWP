ebff89f61c9139a8a914b613c5f37bb742627efd
Luis <64982212+Ltotheois@users.noreply.github.com>
2021-02-14 22:28:10 +0100

    Initial Commit

--- sw.js
+++ sw.js
@@ -1,26 +1,26 @@
-var cacheName = 'LLWP-cache';
+var cacheName = 'cache_llwp';
 var filesToCache = [
-  // '/LLWP/',
-  // '/LLWP/index.html',
+  '/LLWP/',
+  '/LLWP/index.html',
   '/LLWP/docs.html',
-  // '/LLWP/contact.html',
-  // '/LLWP/download.html',
-  // '/LLWP/videos.html',
-  // '/LLWP/LLWP.svg',
-  // '/LLWP/resources/LLWPvideo.mp4',
-  // '/LLWP/resources/LLWPvideo.jpg',
-  // '/LLWP/resources/download/InstallScripts/ubuntu_install.sh',
-  // '/LLWP/resources/download/InstallScripts/windows_install.sh',
-  // '/LLWP/sw.js',
-  // '/LLWP/manifest.json',
-  // '/LLWP/resources/fonts/nunito-sans-v6-latin-900.woff2',
-  // '/LLWP/resources/fonts/nunito-sans-v6-latin-800.woff2',
-  // '/LLWP/resources/fonts/nunito-sans-v6-latin-700.woff2',
-  // '/LLWP/resources/fonts/nunito-sans-v6-latin-600.woff2',
-  // '/LLWP/resources/fonts/nunito-sans-v6-latin-300.woff2',
-  // '/LLWP/resources/fonts/nunito-sans-v6-latin-700italic.woff2',
-  // '/LLWP/resources/fonts/nunito-sans-v6-latin-600italic.woff2',
-  // '/LLWP/resources/fonts/nunito-sans-v6-latin-300italic.woff2',
+  '/LLWP/contact.html',
+  '/LLWP/download.html',
+  '/LLWP/videos.html',
+  '/LLWP/LLWP.svg',
+  '/LLWP/resources/download/LLWPvideo.mp4',
+  '/LLWP/resources/download/LLWPvideo.jpg',
+  '/LLWP/resources/download/InstallScripts/ubuntu_install.sh',
+  '/LLWP/resources/download/InstallScripts/windows_install.bat',
+  '/LLWP/sw.js',
+  '/LLWP/manifest.json',
+  '/LLWP/resources/fonts/nunito-sans-v6-latin-900.woff2',
+  '/LLWP/resources/fonts/nunito-sans-v6-latin-800.woff2',
+  '/LLWP/resources/fonts/nunito-sans-v6-latin-700.woff2',
+  '/LLWP/resources/fonts/nunito-sans-v6-latin-600.woff2',
+  '/LLWP/resources/fonts/nunito-sans-v6-latin-300.woff2',
+  '/LLWP/resources/fonts/nunito-sans-v6-latin-700italic.woff2',
+  '/LLWP/resources/fonts/nunito-sans-v6-latin-600italic.woff2',
+  '/LLWP/resources/fonts/nunito-sans-v6-latin-300italic.woff2'
 ];
 
 /* Start the service worker and cache all of the app's content */
@@ -28,8 +28,9 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(filesToCache);
-    })
+	})
   );
+  self.skipWaiting();
 });
 
 /* Serve cached content when offline */
